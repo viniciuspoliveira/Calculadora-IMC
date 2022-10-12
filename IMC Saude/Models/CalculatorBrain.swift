405b7af521659975a508742f3ae1f9fddcb5fc2f
Vinicius Pinheiro de Oliveira <viniciusp.oliveira@icloud.com>
2022-10-12 14:44:48 -0300

    finishing touches

--- IMC Saude/Models/CalculatorBrain.swift
+++ IMC Saude/Models/CalculatorBrain.swift
@@ -31,10 +31,10 @@ struct CalculatorBrain{
             imc = IMC(value: imcValue, advice: "Você precisa se alimentar melhor", color: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))
                 
         } else if imcValue < 24.9 {
-            imc = IMC(value: imcValue, advice: "Você está parecendo Cristiano Ronaldo! Em Forma!", color: #colorLiteral(red: 0, green: 0.4588235294, blue: 0.8901960784, alpha: 1))
+            imc = IMC(value: imcValue, advice: "Muito bem! Quase um atleta heim!", color: #colorLiteral(red: 0, green: 0.4588235294, blue: 0.8901960784, alpha: 1))
             
         } else {
-            imc = IMC(value: imcValue, advice: "Você precisa comer um pouco menos de besteira. Força!", color: #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1))
+            imc = IMC(value: imcValue, advice: "Vamos diminuir o salgadinho?", color: #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1))
         }
         
     }
